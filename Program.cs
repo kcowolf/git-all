4dea553e8f781645c0121c62695aad85fda424c1
Benjamin Stauffer <kcowolf@gmail.com>
2023-09-13 01:12:47 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using CliWrap;
+using CliWrap.EventStream;
+
+namespace git_all
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var gitArgs = string.Join(' ', args);
+
+            foreach (var directory in Directory.GetDirectories("."))
+            {
+                if (!Directory.Exists(Path.Combine(directory, ".git")))
+                {
+                    continue;
+                }
+
+                Console.WriteLine("\n--------------------------------------------------------------------------------");
+                Console.WriteLine(directory);
+                Console.WriteLine();
+
+                var cmd = Cli.Wrap("git.exe") 
+                    .WithArguments(gitArgs)
+                    .WithWorkingDirectory(directory)
+                    .WithValidation(CommandResultValidation.None);
+
+                await foreach (var cmdEvent in cmd.ListenAsync())
+                {
+                    switch (cmdEvent)
+                    {
+                        case StandardOutputCommandEvent stdOut:
+                            Console.WriteLine($"{stdOut.Text}");
+                            break;
+                        case StandardErrorCommandEvent stdErr:
+                            Console.WriteLine($"{stdErr.Text}");
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
