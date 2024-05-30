354ffbf0c7071808fdad62218e3e47e5618b2950
Benjamin Stauffer <kcowolf@gmail.com>
2024-05-30 17:58:45 -0500

    Run commands in parallel

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using CliWrap;
 using CliWrap.EventStream;
+using System.Text;
 
 namespace git_all
 {
@@ -8,8 +9,10 @@ namespace git_all
         static async Task Main(string[] args)
         {
             var gitArgs = string.Join(' ', args);
+            var directories = Directory.GetDirectories(".");
+            var tasks = new Dictionary<string, Task<string>>();
 
-            foreach (var directory in Directory.GetDirectories("."))
+            foreach (var directory in directories)
             {
                 var gitPath = Path.Combine(directory, ".git");
 
@@ -18,30 +21,46 @@ namespace git_all
                     continue;
                 }
 
-                Console.WriteLine("\n--------------------------------------------------------------------------------");
-                Console.WriteLine(directory);
-                Console.WriteLine();
+                tasks.Add(directory, Run(directory, gitArgs));
+            }
 
-                var cmd = Cli.Wrap("git.exe") 
-                    .WithArguments(gitArgs)
-                    .WithWorkingDirectory(directory)
-                    .WithValidation(CommandResultValidation.None);
-
-                await foreach (var cmdEvent in cmd.ListenAsync())
+            foreach (var directory in directories)
+            {
+                if (tasks.ContainsKey(directory))
                 {
-                    switch (cmdEvent)
-                    {
-                        case StandardOutputCommandEvent stdOut:
-                            Console.WriteLine($"{stdOut.Text}");
-                            break;
-                        case StandardErrorCommandEvent stdErr:
-                            Console.WriteLine($"{stdErr.Text}");
-                            break;
-                    }
+                    Console.WriteLine(await tasks[directory]);
                 }
             }
 
             Console.WriteLine("Done!");
+        }
+
+        private static async Task<string> Run(string directory, string gitArgs)
+        {
+            var output = new StringBuilder();
+            output.AppendLine("\n--------------------------------------------------------------------------------");
+            output.AppendLine(directory);
+            output.AppendLine();
+
+            var cmd = Cli.Wrap("git.exe")
+                .WithArguments(gitArgs)
+                .WithWorkingDirectory(directory)
+                .WithValidation(CommandResultValidation.None);
+
+            await foreach (var cmdEvent in cmd.ListenAsync())
+            {
+                switch (cmdEvent)
+                {
+                    case StandardOutputCommandEvent stdOut:
+                        output.AppendLine($"{stdOut.Text}");
+                        break;
+                    case StandardErrorCommandEvent stdErr:
+                        output.AppendLine($"{stdErr.Text}");
+                        break;
+                }
+            }
+
+            return output.ToString();
         }
     }
 }
